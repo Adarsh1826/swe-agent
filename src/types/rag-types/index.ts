228a0fb58717e5e154baeca01095e8ada3c14899
adarsh singh <adsredmi182615@gmail.com>
2026-02-06 23:53:27 +0530

    added rag

---
+++ src/types/rag-types/index.ts
@@ -0,0 +1,14 @@
+export interface FileLookupInterface{
+    owner:string,
+    repo:string,
+    issueData:{
+      number: number,
+      title: string,
+      body: string,
+      labels?: string[],
+      author: string,
+      url?: string
+
+    }
+
+}
