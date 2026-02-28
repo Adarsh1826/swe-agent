e3633392aac101277a34c09079b3ce4adf949fa3
adarsh singh <adsredmi182615@gmail.com>
2026-02-28 16:00:35 +0530

    fixed

--- src/http/user/routes/route.ts
+++ src/http/user/routes/route.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify";
-import { addApiKey } from "../apis/api";
+import { addApiKey } from "../apis/api.js";
 
 
 export default async function apiRoutes(app: FastifyInstance) {
