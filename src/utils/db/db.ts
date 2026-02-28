6108230876e74e1a361bd935e66ac6db63e860dd
adarsh singh <adsredmi182615@gmail.com>
2026-02-28 16:25:38 +0530

    fixed prsima client issue

--- src/utils/db/db.ts
+++ src/utils/db/db.ts
@@ -1,9 +1,7 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/client.js";
+import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
 
