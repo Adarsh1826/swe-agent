fbfb6342e824d5d61fd58074d656965d7946d33e
ShubhamKashyap1001 <shubh9142996613@gmail.com>
2026-03-03 21:18:21 +0530

    Add apikey

--- src/http/user/routes/route.ts
+++ src/http/user/routes/route.ts
@@ -1,9 +1,24 @@
+// import { FastifyInstance } from "fastify";
+// import { addApiKey } from "../apis/api.js";
+
+
+// export default async function apiRoutes(app: FastifyInstance) {
+//   app.post("/addApiKey", addApiKey);
+// }
+
 import { FastifyInstance } from "fastify";
 import { addApiKey } from "../apis/api.js";
-
+import { getUserApiKeys } from "../apis/get.js";
+import { deleteUserApiKey } from "../apis/delete.js";
 
 export default async function apiRoutes(app: FastifyInstance) {
+
+  // Add API Key
   app.post("/addApiKey", addApiKey);
+
+  // Get User / Guest API Keys
+  app.get("/getUserApiKeys", getUserApiKeys);
+
+  // Delete API Key
+  app.delete("/deleteApiKey/:id", deleteUserApiKey);
 }
-
-
